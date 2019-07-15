ac5741d53edd9570a36f5767020a7a8451c1f5b7
Dmytro Tkachuk <dmitry.tkk@gmail.com>
2019-07-15 18:12:27 +0300

    New package version built and published

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ extras_require['dev'] = (
 setup(
     name='skale.py',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='0.71.0',
+    version='0.72.0',
     description='SKALE client tools',
     long_description_markdown_filename='README.md',
     author='SKALE Labs',
