384e9cff36f190cd30b73b14fce4dac71a675ef9
Dmytro Tkachuk <dmitry.tkk@gmail.com>
2019-08-08 16:15:45 +0300

    use python files for random names vocabulary

--- skale/utils/random_names/generator.py
+++ skale/utils/random_names/generator.py
@@ -1,42 +1,24 @@
-import os
 import random
-
-from skale.utils.helper import read_file
-
-CURRENT_FILE_LOCATION = os.path.dirname(os.path.realpath(__file__))
-
-CONSTELLATIONS_FILENAME = 'constellations.txt'
-CONSTELLATIONS_FILE = os.path.join(CURRENT_FILE_LOCATION, CONSTELLATIONS_FILENAME)
-
-POS_AND_TIME_ADJECTIVES_FILENAME = 'positive-and-time-adjectives.txt'
-POS_AND_TIME_ADJECTIVES_FILE = os.path.join(CURRENT_FILE_LOCATION, POS_AND_TIME_ADJECTIVES_FILENAME)
-
-STARS_FILENAME = 'stars.txt'
-STARS_FILE = os.path.join(CURRENT_FILE_LOCATION, STARS_FILENAME)
-
-SOUND_AND_APPEARANCE_ADJECTIVES_FILENAME = 'sound-and-appearance-adjectives.txt'
-SOUND_AND_APPEARANCE_ADJECTIVES_FILE = os.path.join(CURRENT_FILE_LOCATION,
-                                                    SOUND_AND_APPEARANCE_ADJECTIVES_FILENAME)
+from skale.utils.random_names.vocabulary import CONSTELLATIONS, STARS, \
+    SOUND_AND_APPEARANCE_ADJECTIVES, POSITIVE_AND_TIME_ADJECTIVES
 
 
 def generate_random_node_name():
-    return generate_name(CONSTELLATIONS_FILENAME, POS_AND_TIME_ADJECTIVES_FILE)
+    return generate_name(CONSTELLATIONS, POSITIVE_AND_TIME_ADJECTIVES)
 
 
 def generate_random_schain_name():
-    return generate_name(STARS_FILE, SOUND_AND_APPEARANCE_ADJECTIVES_FILE)
+    return generate_name(STARS, SOUND_AND_APPEARANCE_ADJECTIVES)
 
 
-def generate_name(noun_file, adjective_file):
-    noun = get_random_word_from_file(noun_file)
-    adjective = get_random_word_from_file(adjective_file)
+def generate_name(noun_dict, adjective_dict):
+    noun = get_random_word_from_dict(noun_dict)
+    adjective = get_random_word_from_dict(adjective_dict)
     return f'{adjective}-{noun}'
 
 
-def get_random_word_from_file(filename):
-    txt = read_file(filename)
-    words_list = txt.splitlines()
-    return random.choice(words_list).lower().replace(" ", "-")
+def get_random_word_from_dict(vocabulary_dict):
+    return random.choice(vocabulary_dict).lower().replace(" ", "-")
 
 
 if __name__ == "__main__":
