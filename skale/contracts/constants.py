145dfff0534b65ee4cc6222a95362c2a328d5a38
badrogger <ivan@skalelabs.com>
2019-09-19 19:04:18 +0300

    SKALE-1561 Add check_period getter/setter into skale.py
    Add getter setter to checkTime variable from skale-manager Constants contract

--- skale/contracts/constants.py
+++ skale/contracts/constants.py
@@ -8,3 +8,17 @@ class Constants(BaseContract):
         op = self.contract.functions.setPeriods(new_reward_period, new_delta_period)
         tx = sign_and_send(self.skale, op, GAS['set_periods'], wallet)
         return {'tx': tx}
+
+    def get_reward_period(self):
+        return self.contract.functions.rewardPeriod().call()
+
+    def get_delta_period(self):
+        return self.contract.functions.deltaPeriod().call()
+
+    def set_check_time(self, new_check_time, wallet):
+        op = self.contract.functions.setCheckTime(new_check_time)
+        tx = sign_and_send(self.skale, op, GAS['set_check_time'], wallet)
+        return {'tx': tx}
+
+    def get_check_time(self):
+        return self.contract.functions.checkTime().call()
