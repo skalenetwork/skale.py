14038a91c18e06716eadc2dae95b744c13fa7263
Dmytro Tkachuk <dmitry.tkk@gmail.com>
2019-10-07 15:53:15 +0300

    SKALE-1574 add checks to publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+: "${PIP_USERNAME?Need to set PIP_USERNAME}"
+: "${PIP_PASSWORD?Need to set PIP_PASSWORD}"
+
 if [ $TEST = 1 ]; then
     twine upload --repository testpypi dist/*
 else
