cacaa33edc5202027363a4c2fba5bce4aefa4f96
Chadwick Strange <cstrangedk@users.noreply.github.com>
2019-11-27 16:56:17 +0100

    Fix classifier reference

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU Affero General Public License v3 (AGPLv3)',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3.6',
     ],
