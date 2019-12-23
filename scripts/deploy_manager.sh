5345ef9f49d61cc40246296c141a02e4c9ca2047
Dmytro Tkachuk <dmitry.tkk@gmail.com>
2019-12-23 18:11:00 +0200

    SKALE-1897 new tests pipeline

---
+++ scripts/deploy_manager.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+: "${MANAGER_BRANCH?Need to set MANAGER_BRANCH}"
+: "${ETH_PRIVATE_KEY?Need to set ETH_PRIVATE_KEY}"
+: "${ENDPOINT_HTTP?Need to set ENDPOINT_HTTP}"
+
+export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+docker run -ti -e PRIVATE_KEY=$ETH_PRIVATE_KEY -e ENDPOINT=$ENDPOINT_HTTP \
+    -v $DIR/contracts_data:/usr/src/manager/data \
+    skalelabshub/skale-manager:$MANAGER_BRANCH-latest \
+    npx truffle migrate --network unique
+
+cp $DIR/contracts_data/unique.json $DIR/../test_abi.json
