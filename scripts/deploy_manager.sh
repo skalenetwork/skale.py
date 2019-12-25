4bf149f78e2c8175268ac6ee8d962460bee002b9
badrogger <ivan@skalelabs.com>
2019-12-25 15:32:51 +0200

    Add --network to skale-manager container

--- scripts/deploy_manager.sh
+++ scripts/deploy_manager.sh
@@ -10,13 +10,12 @@ set -e
 docker run -d -p 8545:8545 --name ganache trufflesuite/ganache-cli:latest  \
     --account="${ETH_PRIVATE_KEY},100000000000000000000000000" -l 80000000
 
-docker ps -a
-
 echo "$PASSWORD" | docker login --username $USERNAME --password-stdin
 
 export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 docker run -ti \
+    --network host
     -v $DIR/contracts_data:/usr/src/manager/data \
     skalelabshub/skale-manager:$MANAGER_BRANCH-latest \
     npx truffle migrate --network test
