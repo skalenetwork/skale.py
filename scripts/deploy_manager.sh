c53e6d4dea3d18291fa78e6860eba94d679f92af
badrogger <ivan@skalelabs.com>
2019-12-25 16:08:20 +0200

    Change -it and --network order

--- scripts/deploy_manager.sh
+++ scripts/deploy_manager.sh
@@ -3,20 +3,20 @@
 set -e
 
 : "${ETH_PRIVATE_KEY?Need to set ETH_PRIVATE_KEY}"
-: "${USERNAME?Need to set USERNAME}"
-: "${PASSWORD?Need to set PASSWORD}"
+: "${DOCKER_USERNAME?Need to set DOCKER_USERNAME}"
+: "${DOCKER_PASSWORD?Need to set DOCKER_PASSWORD}"
 : "${MANAGER_BRANCH?Need to set MANAGER_BRANCH}"
 
 docker run -d -p 8545:8545 --name ganache trufflesuite/ganache-cli:latest  \
     --account="${ETH_PRIVATE_KEY},100000000000000000000000000" -l 80000000
 
-echo "$PASSWORD" | docker login --username $USERNAME --password-stdin
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 
 export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker run -ti \
+docker run \
     -v $DIR/contracts_data:/usr/src/manager/data \
-    --network host
+    --network host -it \
     skalelabshub/skale-manager:$MANAGER_BRANCH-latest \
     npx truffle migrate --network test
 
