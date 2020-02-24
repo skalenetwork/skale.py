6d72b4a87e0b6d7589d9258f0014cdcdfb6f27de
Dmytro Tkachuk <dmitry.tkk@gmail.com>
2020-02-24 15:22:17 +0200

    Remove docker login from deploy_manager

--- scripts/deploy_manager.sh
+++ scripts/deploy_manager.sh
@@ -3,14 +3,10 @@
 set -e
 
 : "${ETH_PRIVATE_KEY?Need to set ETH_PRIVATE_KEY}"
-: "${DOCKER_USERNAME?Need to set DOCKER_USERNAME}"
-: "${DOCKER_PASSWORD?Need to set DOCKER_PASSWORD}"
 : "${MANAGER_BRANCH?Need to set MANAGER_BRANCH}"
 
 docker run -d --network host --name ganache trufflesuite/ganache-cli:v6.8.1-beta.0 \
     --account="${ETH_PRIVATE_KEY},100000000000000000000000000" -l 80000000
-
-echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 
 export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
