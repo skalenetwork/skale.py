0a9517cb09cc1f19ac58df9d54a8ef9247a2a5f7
Dmytro Tkachuk <dmitry.tkk@gmail.com>
2020-02-24 15:34:15 +0200

    Remove -ti option from docker run

--- scripts/deploy_manager.sh
+++ scripts/deploy_manager.sh
@@ -12,7 +12,7 @@ export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 docker run \
     -v $DIR/contracts_data:/usr/src/manager/data \
-    --network host -it \
+    --network host \
     skalenetwork/skale-manager:$MANAGER_BRANCH-latest \
     npx truffle migrate --network test
 
