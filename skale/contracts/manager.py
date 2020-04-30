49fda15598248aa6714eb33694af9880e5620929
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2020-04-30 12:39:40 +0300

    SKALE-2495 Update decorator

--- skale/contracts/manager.py
+++ skale/contracts/manager.py
@@ -134,6 +134,6 @@ class Manager(BaseContract):
     def delete_node_by_root(self, node_id):
         return self.contract.functions.deleteNodeByRoot(node_id)
 
-    @transaction_method
+    @transaction_method(GAS['node_exit'])
     def node_exit(self, node_id):
         return self.contract.functions.nodeExit(node_id)
