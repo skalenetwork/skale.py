80fde6d696f69b4783236608d63731208b119c41
badrogger <ivan@skalelabs.com>
2020-05-25 16:00:29 +0300

    SKALE-2566 Fix set_penalty transaction_method decorator

--- skale/contracts/delegation/slashing_table.py
+++ skale/contracts/delegation/slashing_table.py
@@ -6,7 +6,7 @@ from skale.utils.constants import GAS
 class SlashingTable(BaseContract):
     """ Wrapper for SlashingTable.sol functions """
 
-    @transaction_method(GAS['set_penalty'])
+    @transaction_method(gas_limit=GAS['set_penalty'])
     def set_penalty(self, offense, penalty) -> TxRes:
         """ Set slashing penalty
         :param offense: reason of slashing
