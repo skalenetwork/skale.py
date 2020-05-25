31235163d17a09227967aecdb602ae833b31600a
badrogger <ivan@skalelabs.com>
2020-05-25 15:28:55 +0300

    SKALE-2596 Fix flake8 error

--- skale/utils/constants.py
+++ skale/utils/constants.py
@@ -57,11 +57,8 @@ GAS = {
     'get_and_update_locked_amount': 3000000,
     'request_undelegation': 3000000,
     'set_penalty': 3000000,
-    'disable_whitelist': 3000000
-}
-
-
-    'set_launch_timestamp': 200000,
+    'disable_whitelist': 3000000,
+    'set_launch_timestamp': 200000
 }
 
 LONG_LINE = '=' * 100
