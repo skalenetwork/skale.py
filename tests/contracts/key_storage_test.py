ebb08932c25b4eb0279338e6d06de022471bc929
Oleh Nikolaiev <oleg@skalelabs.com>
2020-06-26 15:07:29 +0300

    SKALE-2810 fix tests

--- tests/contracts/key_storage_test.py
+++ tests/contracts/key_storage_test.py
@@ -2,21 +2,23 @@ from tests.constants import (DEFAULT_SCHAIN_ID, DEFAULT_SCHAIN_NAME)
 
 
 def test_get_broadcasted_data(skale):
-    broadcasted_data = skale.key_storage.get_broadcasted_data(DEFAULT_SCHAIN_ID, 0).call()
+    group_index = b'e629fa6598d732768f7c726b4b621285'
+    broadcasted_data = skale.key_storage.get_broadcasted_data(group_index, 0).call()
     assert broadcasted_data[0] == [["", ""], ""]
     assert broadcasted_data[1] == [0, 0, 0, 0]
 
 
 def test_get_bls_public_key(skale):
-    group_id = skale.web3.sha3(text=DEFAULT_SCHAIN_NAME)
-    assert skale.key_storage.get_bls_public_key(group_id, 0).call() == [0, 0, 0, 0]
+    group_index = b'e629fa6598d732768f7c726b4b621285'
+    assert skale.key_storage.get_bls_public_key(group_index, 0).call() == [0, 0, 0, 0]
 
 
 def test_get_previous_groups_public_key(skale):
-    group_id = skale.web3.sha3(text=DEFAULT_SCHAIN_NAME)
-    public_key = skale.key_storage.get_previous_public_key(group_id).call()
+    group_index = b'e629fa6598d732768f7c726b4b621285'
+    public_key = skale.key_storage.get_previous_public_key(group_index).call()
     assert len(public_key) == 4
 
 
 def test_get_group_public_key(skale):
-    assert skale.key_storage.get_groups_public_key(DEFAULT_SCHAIN_ID).call() == [0, 0, 0, 0]
+    group_index = b'e629fa6598d732768f7c726b4b621285'
+    assert skale.key_storage.get_groups_public_key(group_index).call() == [0, 0, 0, 0]
