ba074804569b4300d795593900126f0865504dc9
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2020-07-13 13:20:32 +0300

    SKALE-2843 Add get_active_schains call

--- skale/contracts/schains_internal.py
+++ skale/contracts/schains_internal.py
@@ -56,3 +56,6 @@ class SChainsInternal(BaseContract):
     def is_schain_exist(self, name):
         id_ = self.schains.name_to_id(name)
         return self.contract.functions.isSchainExist(id_).call()
+
+    def get_active_schains(self, node_id):
+        return self.contract.functions.getActiveSchains(node_id).call()
