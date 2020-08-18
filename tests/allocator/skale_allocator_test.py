3b2176b2ed2c368790fd7b9d441ce6915fd3b9ed
Dmytro <dmitry.tkk@gmail.com>
2020-08-18 16:29:25 +0300

    SKALE-3068 Fix init allocator test

--- tests/allocator/skale_allocator_test.py
+++ tests/allocator/skale_allocator_test.py
@@ -4,12 +4,13 @@ from skale import SkaleAllocator
 from skale.wallets import Web3Wallet
 from skale.utils.web3_utils import init_web3
 from skale.allocator.contracts_info import CONTRACTS_INFO
-from tests.constants import ENDPOINT, TEST_ABI_FILEPATH, ETH_PRIVATE_KEY
+from tests.constants import ENDPOINT, TEST_ALLOCATOR_ABI_FILEPATH, ETH_PRIVATE_KEY
 
 
 def test_init_allocator():
     web3 = init_web3(ENDPOINT)
     wallet = Web3Wallet(ETH_PRIVATE_KEY, web3)
-    skale_allocator = SkaleAllocator(ENDPOINT, TEST_ABI_FILEPATH, wallet, provider_timeout=20)
+    skale_allocator = SkaleAllocator(ENDPOINT, TEST_ALLOCATOR_ABI_FILEPATH, wallet,
+                                     provider_timeout=20)
     lib_contracts = skale_allocator._SkaleBase__contracts
     assert len(lib_contracts) == len(CONTRACTS_INFO)
