e6a4aa58d527793cac5fd1f6f0d228381ca9b160
Dmytro <dmitry.tkk@gmail.com>
2020-08-25 13:28:38 +0300

    SKALE-3031 Update allocator test, update allocator image

--- tests/allocator/allocator_test.py
+++ tests/allocator/allocator_test.py
@@ -11,6 +11,10 @@ TEST_VESTING_INTERVAL = 6
 TEST_CAN_DELEGATE = False
 TEST_IS_TERMINATABLE = True
 
+TEST_START_MONTH = int("{:%s}".format(datetime.date(2020, 10, 1)))
+TEST_FULL_AMOUNT = 10 ** 6
+TEST_LOCKUP_AMOUNT = 10 ** 5
+
 POLL_INTERVAL = 2
 
 
@@ -26,6 +30,18 @@ def _add_test_plan(skale_allocator, wait_for):
     )
     event = catch_event(skale_allocator.allocator.contract.events.PlanCreated)
     return event.args['id']
+
+
+def _add_plan_and_connect_beneficiary(skale_allocator, wallet):
+    plan_id = _add_test_plan(skale_allocator, False)
+    skale_allocator.allocator.connect_beneficiary_to_plan(
+        beneficiary_address=wallet.address,
+        plan_id=plan_id,
+        start_month=TEST_START_MONTH,
+        full_amount=TEST_FULL_AMOUNT,
+        lockup_amount=TEST_LOCKUP_AMOUNT,
+        wait_for=True
+    )
 
 
 def test_is_beneficiary_registered(skale_allocator):
@@ -51,22 +67,14 @@ def test_add_plan(skale_allocator):
 
 def test_connect_beneficiary_to_plan(skale_allocator):
     wallet = generate_wallet(skale_allocator.web3)
-    d = datetime.date(2020, 10, 1)
-    start_month = int("{:%s}".format(d))
-
-    full_amount = 10 ** 6
-    lockup_amount = 10 ** 5
-
     assert not skale_allocator.allocator.is_beneficiary_registered(wallet.address)
     plan_id = _add_test_plan(skale_allocator, False)
-
     skale_allocator.allocator.connect_beneficiary_to_plan(
         beneficiary_address=wallet.address,
         plan_id=plan_id,
-        start_month=start_month,
-        full_amount=full_amount,
-        lockup_amount=lockup_amount,
+        start_month=TEST_START_MONTH,
+        full_amount=TEST_FULL_AMOUNT,
+        lockup_amount=TEST_LOCKUP_AMOUNT,
         wait_for=True
     )
-
     assert skale_allocator.allocator.is_beneficiary_registered(wallet.address)
--- tests/allocator/escrow_test.py
+++ tests/allocator/escrow_test.py
@@ -1,11 +1,22 @@
 """ Tests for skale/allocator/escrow.py """
 
+import pytest
+from skale.wallets.web3_wallet import generate_wallet
+from skale.utils.account_tools import send_ether
+
+from tests.allocator.allocator_test import _add_plan_and_connect_beneficiary
 from tests.constants import (D_DELEGATION_INFO, D_VALIDATOR_ID,
                              D_DELEGATION_AMOUNT, D_DELEGATION_PERIOD)
 
 
-# @pytest.mark.skip(reason="not ready yet")
+@pytest.mark.skip(reason="not ready yet")
 def test_delegate(skale_allocator):
+    main_wallet = skale_allocator.wallet
+    wallet = generate_wallet(skale_allocator.web3)
+    send_ether(skale_allocator.web3, main_wallet, wallet.address, 0.1)
+    _add_plan_and_connect_beneficiary(skale_allocator, wallet)
+
+    skale_allocator.wallet = wallet
     skale_allocator.escrow.delegate(
         validator_id=D_VALIDATOR_ID,
         amount=D_DELEGATION_AMOUNT,
@@ -13,3 +24,4 @@ def test_delegate(skale_allocator):
         info=D_DELEGATION_INFO,
         wait_for=True
     )
+    skale_allocator.wallet = main_wallet
