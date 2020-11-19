9136765333c05455e4463b685ef1f06e0ba308ff
Dmytro <dmitry.tkk@gmail.com>
2020-11-19 15:01:52 +0200

    SKALE-3525 Split manager and allocator pipelines

--- scripts/run_allocator_tests.sh
+++ scripts/run_allocator_tests.sh
@@ -2,6 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJECT_DIR=$(dirname $DIR)
+export ENDPOINT=${ENDPOINT:-http://localhost:8545}
 
 python $PROJECT_DIR/tests/allocator/provision_contracts.py
 py.test --cov=$PROJECT_DIR/ $PROJECT_DIR/tests/allocator
