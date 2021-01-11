0c800fecdb9555b66fac7b91bc1b6ae1786ec830
badrogger <ivan@skalelabs.com>
2021-01-11 17:54:16 +0200

    SKALE-3699 Update sgx wallet according to the new sgx.py

--- tests/wallets/utils.py
+++ tests/wallets/utils.py
@@ -1,5 +1,6 @@
 from hexbytes import HexBytes
-from eth_account.datastructures import AttributeDict
+from eth_account.datastructures import SignedTransaction, SignedMessage
+from sgx.sgx import Account
 from skale.utils.web3_utils import (
     private_key_to_address,
     private_key_to_public,
@@ -18,32 +19,33 @@ class SgxClient:
         pass
 
     def generate_key(self):
-        return AttributeDict({
-            'name': 'NEK:aaabbb',
-            'address': ADDRESS,
-            'public_key': PUBLIC_KEY
-        })
+        return Account(
+            name='NEK:aaabbb',
+            address=ADDRESS,
+            public_key=PUBLIC_KEY
+        )
 
     def get_account(self, key_name):
-        return AttributeDict({
-            'address': ADDRESS,
-            'public_key': PUBLIC_KEY
-        })
+        return Account(
+            name='NEK:aaabbb',
+            address=ADDRESS,
+            public_key=PUBLIC_KEY
+        )
 
     def sign(self, transaction_dict, key_name):
-        return AttributeDict({
-            'rawTransaction': HexBytes('0x000000000000'),
-            'hash': HexBytes('0x000000000000'),
-            'r': 100000000000,
-            's': 100000000000,
-            'v': 37,
-        })
+        return SignedTransaction(
+            rawTransaction=HexBytes('0x000000000000'),
+            hash=HexBytes('0x000000000000'),
+            r=100000000000,
+            s=100000000000,
+            v=37
+        )
 
     def sign_hash(self, message, key_name, chain_id):
-        return AttributeDict({
-            'messageHash': HexBytes('0x31323331'),
-            'r': 123,
-            's': 123,
-            'v': 27,
-            'signature': HexBytes('0x6161616161613131313131')
-        })
+        return SignedMessage(
+            messageHash=HexBytes('0x31323331'),
+            r=123,
+            s=123,
+            v=27,
+            signature=HexBytes('0x6161616161613131313131')
+        )
