f0bd40b3d2b7ca9c15ae7db12df43c9a7ad1d671
badrogger <ivan@skalelabs.com>
2021-06-07 17:55:32 +0300

    SKALE-3350 Fix insufficient balance test

--- tests/transaction_tools_test.py
+++ tests/transaction_tools_test.py
@@ -116,7 +116,7 @@ def test_run_tx_with_retry_insufficient_balance(skale):
         max_retries=retries_number,
     )
 
-    assert sender_skale.wallet.sign_and_send.call_count == retries_number
+    assert sender_skale.wallet.sign_and_send.call_count == 0
     assert sender_skale.wallet.wait.call_count == 0
 
 
