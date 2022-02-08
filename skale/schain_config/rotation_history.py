674c4ed1ece58ef62fa58552d2c4f4e6cc4b3b29
Dmytro <dmitry.tkk@gmail.com>
2022-02-08 18:22:32 +0200

    SKALE-4943 Add function for multiple rotated nodes

--- skale/schain_config/rotation_history.py
+++ skale/schain_config/rotation_history.py
@@ -41,7 +41,6 @@ def get_previous_schain_groups(skale, schain_name: str) -> dict:
     current_public_key = skale.key_storage.get_common_public_key(group_id)
 
     rotation = skale.node_rotation.get_rotation_obj(schain_name)
-    rotation_delay = skale.constants_holder.get_rotation_delay()
 
     logger.info(f'Rotation data for {schain_name}: {rotation}')
 
@@ -50,7 +49,7 @@ def get_previous_schain_groups(skale, schain_name: str) -> dict:
         return node_groups
 
     _add_previous_schain_rotations_state(
-        skale, node_groups, rotation, schain_name, previous_public_keys, rotation_delay)
+        skale, node_groups, rotation, schain_name, previous_public_keys)
     return node_groups
 
 
@@ -72,6 +71,7 @@ def _add_current_schain_state(
         current_nodes[node_id] = RotationNodeData(index, node_id, public_key)
 
     node_groups[rotation.rotation_counter] = {
+        'rotation': None,
         'nodes': current_nodes,
         'finish_ts': None,
         'bls_public_key': _compose_bls_public_key_info(current_public_key)
@@ -84,7 +84,6 @@ def _add_previous_schain_rotations_state(
     rotation: Rotation,
     schain_name: str,
     previous_public_keys: list,
-    rotation_delay: int
 ) -> dict:
     """
     Internal function, handles rotations from (rotation_counter - 2) to 0 and adds them to the
@@ -126,7 +125,13 @@ def _add_previous_schain_rotations_state(
         else:
             bls_public_key, node_finish_ts = None, None
 
+        logger.info(f'Adding rotation: {previous_node_id} -> {latest_exited_node_id}')
+
         node_groups[rotation_id] = {
+            'rotation': {
+                'leaving_node_id': previous_node_id,
+                'new_node_id': latest_exited_node_id
+            },
             'nodes': nodes,
             'finish_ts': node_finish_ts,
             'bls_public_key': bls_public_key
@@ -153,3 +158,19 @@ def _compose_bls_public_key_info(bls_public_key: str) -> dict:
         'blsPublicKey2': str(bls_public_key[1][0]),
         'blsPublicKey3': str(bls_public_key[1][1])
     }
+
+
+def get_new_nodes_list(skale: Skale, name: str, node_groups) -> list:
+    """Returns list of new nodes in for the latest rotation"""
+    logger.info(f'Getting new nodes list for chain {name}')
+    rotation = skale.node_rotation.get_rotation_obj(name)
+    new_nodes = []
+    for index in node_groups:
+        past_rotation = node_groups[index]['rotation']
+        if not past_rotation:
+            continue
+        new_nodes.append(past_rotation['new_node_id'])
+        if rotation.leaving_node_id == past_rotation['leaving_node_id']:
+            break
+    logger.info(f'New nodes list for chain {name}: {new_nodes}')
+    return new_nodes
