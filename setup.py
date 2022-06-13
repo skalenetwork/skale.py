262f497f2afe210319d69b30f3c482ce8e284c7a
badrogger <ivan@skalelabs.com>
2022-06-13 14:11:50 +0000

    SKALE-4904 Bump version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ extras_require['dev'] = (
 
 setup(
     name='skale.py',
-    version='5.7',
+    version='5.8',
     description='SKALE client tools',
     long_description_markdown_filename='README.md',
     author='SKALE Labs',
