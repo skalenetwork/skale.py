10f48c134bd09458e9f6e30297505ed3015a5ade
Dmytro Stebaiev <dimastebaev@gmail.com>
2023-05-17 17:48:16 +0300

    Add stop on error

--- scripts/run_allocator_tests.sh
+++ scripts/run_allocator_tests.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJECT_DIR=$(dirname $DIR)
 export ENDPOINT=${ENDPOINT:-http://localhost:8545}
