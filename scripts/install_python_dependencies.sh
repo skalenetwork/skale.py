5abfe027a9b748df7936d1691602587b7f0e6666
badrogger <ivan@skalelabs.com>
2023-06-02 10:42:18 +0000

    Remove redundant dependency installation

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -6,4 +6,3 @@ python -m pip install --upgrade pip
 pip install -e .
 pip install -e .[dev]
 pip install codecov pytest-cov
-pip install git+git://github.com/skalenetwork/sgx.py.git@add-python-3.11-support#egg=sgx.py
