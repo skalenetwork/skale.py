3d73289ebaca8fd03aa59515ecacb0a4e995afd0
badrogger <ivan@skalelabs.com>
2023-10-27 16:43:11 +0000

    Remove legacy RPCWallet

--- skale/wallets/__init__.py
+++ skale/wallets/__init__.py
@@ -3,6 +3,5 @@
 from skale.wallets.common import BaseWallet
 from skale.wallets.ledger_wallet import LedgerWallet
 from skale.wallets.redis_wallet import RedisWallet
-from skale.wallets.rpc_wallet import RPCWallet
 from skale.wallets.sgx_wallet import SgxWallet
 from skale.wallets.web3_wallet import Web3Wallet
