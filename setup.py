d5574e0fac19d5095f5541ce2bdae3765f925dee
Dmytro Stebaiev <dimastebaev@gmail.com>
2024-04-04 19:02:28 +0300

    Update skale-contracts

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         "pyyaml==6.0",
         "redis==4.4.4",
         "sgx.py==0.9dev2",
-        "skale-contracts==1.0.1a4",
+        "skale-contracts==1.0.1a5",
         "typing-extensions==4.9.0",
         "web3==6.13.0"
     ],
