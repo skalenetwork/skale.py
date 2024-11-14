ea80c9082821a79bd0f54526e8f6de4580bab5e6
Oleksandr Salamatov <oleksandrsalamatov@skalelabs.com>
2024-11-14 13:43:15 +0200

    add full config controller support

---
+++ skale/contracts/config_controller/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+from skale.contracts.base_contract import BaseContract, transaction_method
+from skale.contracts.config_controller.config_controller import ConfigController
+
+__all__ = [
+    'ConfigController'
+]
