d3dfe83bba917b12b7673005e7f1b9c23b287f69
EvgeniyZh <evgeniy@skalelabs.com>
2024-12-04 18:56:34 +0000

    fix

--- skale/__init__.py
+++ skale/__init__.py
@@ -9,12 +9,4 @@ from skale.skale_manager import SkaleManager
 from skale.skale_manager import SkaleManager as Skale  # todo: deprecated naming, will be removed in skale.py v5
 
 from skale.skale_allocator import SkaleAllocator
-# from skale.contracts.ima_mainnet import Linker
-# from skale.contracts.ima_schain.token_manager_erc20 import TokenManagerERC20
-# from skale.contracts.ima import DepositBoxEth
-# from skale.contracts.ima import DepositBoxERC20
-# from skale.contracts.ima.schain.token_manager_erc20 import TokenManagerERC20
-# from skale.contracts.ima.schain.token_manager_erc721 import TokenManagerERC721
-# from skale.contracts.ima.schain.token_manager_erc721_wmt import TokenManagerERC721WithMetadata
-# from skale.contracts.ima.schain.token_manager_erc1155 import TokenManagerERC1155
-# from skale.contracts.ima.schain.token_manager_eth import TokenManagerETH
+from skale.skale_ima import SkaleIm
