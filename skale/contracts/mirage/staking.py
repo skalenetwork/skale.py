c8bcccfbd68422edd2672d3d7c148ec5dcd76be8
Dmytro <dmitry.tkk@gmail.com>
2025-07-13 18:49:19 +0100

    Add staking functions

--- skale/contracts/mirage/staking.py
+++ skale/contracts/mirage/staking.py
@@ -17,8 +17,63 @@
 #   You should have received a copy of the GNU Affero General Public License
 #   along with SKALE.py.  If not, see <https://www.gnu.org/licenses/>.
 
-from skale.contracts.base_contract import BaseContract
+from typing import List
+from eth_typing import ChecksumAddress
+from web3.types import Wei
+
+from skale.contracts.base_contract import BaseContract, transaction_method
+from skale.types.node import NodeId
 
 
 class Staking(BaseContract):
-    pass
+    def get_node_share(self, node: NodeId) -> int:
+        return self.contract.functions.getNodeShare(node).call()
+
+    def get_staked_amount(self) -> int:
+        return self.contract.functions.getStakedAmount().call()
+
+    def get_staked_to_node_amount(self, node: NodeId) -> int:
+        return self.contract.functions.getStakedToNodeAmount(node).call()
+
+    def get_staked_nodes(self) -> List[NodeId]:
+        return self.contract.functions.getStakedNodes().call()
+
+    def get_earned_fee_amount(self, node: NodeId) -> int:
+        return self.contract.functions.getEarnedFeeAmount(node).call()
+
+    def get_staked_amount_for(self, holder: ChecksumAddress) -> int:
+        return self.contract.functions.getStakedAmountFor(holder).call()
+
+    def get_staked_nodes_for(self, holder: ChecksumAddress) -> List[NodeId]:
+        return self.contract.functions.getStakedNodesFor(holder).call()
+
+    def get_staked_to_node_amount_for(self, node: NodeId, holder: ChecksumAddress) -> int:
+        return self.contract.functions.getStakedToNodeAmountFor(node, holder).call()
+
+    @transaction_method
+    def stake(self, node: NodeId, value: Wei):
+        return self.contract.functions.stake(node, value=value)
+
+    @transaction_method
+    def retrieve(self, node: NodeId, value: int):
+        return self.contract.functions.retrieve(node, value)
+
+    @transaction_method
+    def set_fee_rate(self, fee_rate: int):
+        return self.contract.functions.setFeeRate(fee_rate)
+
+    @transaction_method
+    def claim_fee(self, to: ChecksumAddress, amount: int):
+        return self.contract.functions.claimFee(to, amount)
+
+    @transaction_method
+    def claim_all_fee(self, to: ChecksumAddress):
+        return self.contract.functions.claimAllFee(to)
+
+    @transaction_method
+    def disable(self, node: NodeId):
+        return self.contract.functions.disable(node)
+
+    @transaction_method
+    def enable(self, node: NodeId):
+        return self.contract.functions.enable(node)
