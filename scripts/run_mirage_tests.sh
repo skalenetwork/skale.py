352b0f3b249d25cf422f6feb5d1b6da5be6dfb20
Oleh <oleg@skalelabs.com>
2025-07-14 19:00:48 +0100

    fix tests

--- scripts/run_mirage_tests.sh
+++ scripts/run_mirage_tests.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJECT_DIR=$(dirname $DIR)
 
 export ENDPOINT=${ENDPOINT:-http://localhost:8545}
-export MIRAGE_CONTRACTS=$(bash $PROJECT_DIR/helper-scripts/helper.sh mirage_address)
+export MIRAGE_CONTRACTS=$(bash $PROJECT_DIR/helper-scripts/helper.sh fair_address)
 
-echo "Running Mirage tests with contracts at $MIRAGE_CONTRACTS"
+echo "Running Fair tests with contracts at $FAIR_CONTRACTS"
 pytest $PROJECT_DIR/tests/mirage
