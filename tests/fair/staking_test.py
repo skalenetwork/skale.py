c63b1bc0019d3fb977d95844ceb0890013fa1bff
Dmytro <dmitry.tkk@gmail.com>
2025-08-21 17:15:34 +0100

    Update to the latest fair manager, add new staking functions

--- tests/fair/staking_test.py
+++ tests/fair/staking_test.py
@@ -5,11 +5,11 @@ import pytest
 def test_stake_and_retrieve(fair, fair_active_nodes):
     node = fair.nodes.get_by_address(fair_active_nodes[0].address)
     node_id = node.id
-    
+
     initial_staked_amount = fair.staking.get_staked_amount()
     initial_staked_nodes = fair.staking.get_staked_nodes()
     initial_node_share = fair.staking.get_node_share(node_id)
-    
+
     assert isinstance(initial_staked_amount, int)
     assert isinstance(initial_staked_nodes, list)
     assert isinstance(initial_node_share, int)
@@ -21,11 +21,11 @@ def test_stake_and_retrieve(fair, fair_active_nodes):
 def test_stake_to_node(fair, fair_active_nodes):
     node = fair.nodes.get_by_address(fair_active_nodes[0].address)
     node_id = node.id
-    
+
     staked_amount = fair.staking.get_staked_amount()
     staked_nodes = fair.staking.get_staked_nodes()
     node_share = fair.staking.get_node_share(node_id)
-    
+
     assert isinstance(staked_amount, int)
     assert isinstance(staked_nodes, list)
     assert isinstance(node_share, int)
@@ -34,10 +34,10 @@ def test_stake_to_node(fair, fair_active_nodes):
 @pytest.mark.parametrize('number_of_nodes', [2])
 def test_get_staked_amount_for_holder(fair, fair_active_nodes):
     holder_address = fair_active_nodes[0].address
-    
+
     staked_amount = fair.staking.get_staked_amount_for(holder_address)
     staked_nodes = fair.staking.get_staked_nodes_for(holder_address)
-    
+
     assert isinstance(staked_amount, int)
     assert isinstance(staked_nodes, list)
     assert staked_amount >= 0
@@ -49,9 +49,9 @@ def test_get_staked_to_node_amount_for(fair, fair_active_nodes):
     node = fair.nodes.get_by_address(fair_active_nodes[0].address)
     node_id = node.id
     holder_address = fair_active_nodes[1].address
-    
+
     staked_to_node = fair.staking.get_staked_to_node_amount_for(node_id, holder_address)
-    
+
     assert isinstance(staked_to_node, int)
     assert staked_to_node >= 0
 
@@ -60,9 +60,9 @@ def test_get_staked_to_node_amount_for(fair, fair_active_nodes):
 def test_fee_related_functions(fair, fair_active_nodes):
     node = fair.nodes.get_by_address(fair_active_nodes[0].address)
     node_id = node.id
-    
+
     earned_fee = fair.staking.get_earned_fee_amount(node_id)
-    
+
     assert isinstance(earned_fee, int)
     assert earned_fee >= 0
 
@@ -71,9 +71,9 @@ def test_fee_related_functions(fair, fair_active_nodes):
 def test_node_share_calculation(fair, fair_active_nodes):
     node = fair.nodes.get_by_address(fair_active_nodes[0].address)
     node_id = node.id
-    
+
     node_share = fair.staking.get_node_share(node_id)
-    
+
     assert isinstance(node_share, int)
     assert node_share >= 0
 
@@ -81,7 +81,7 @@ def test_node_share_calculation(fair, fair_active_nodes):
 def test_staking_view_functions_with_no_nodes(fair):
     staked_amount = fair.staking.get_staked_amount()
     staked_nodes = fair.staking.get_staked_nodes()
-    
+
     assert isinstance(staked_amount, int)
     assert isinstance(staked_nodes, list)
     assert staked_amount == 0
@@ -92,8 +92,35 @@ def test_staking_view_functions_with_no_nodes(fair):
 def test_get_staked_to_node_amount(fair, fair_active_nodes):
     node = fair.nodes.get_by_address(fair_active_nodes[0].address)
     node_id = node.id
-    
+
     staked_to_node = fair.staking.get_staked_to_node_amount(node_id)
-    
+
     assert isinstance(staked_to_node, int)
     assert staked_to_node >= 0
+
+
+@pytest.mark.parametrize('number_of_nodes', [1])
+def test_get_node_fee_rate(fair, fair_active_nodes):
+    node = fair.nodes.get_by_address(fair_active_nodes[0].address)
+    node_id = node.id
+
+    fee_rate = fair.staking.get_node_fee_rate(node_id)
+
+    assert isinstance(fee_rate, int)
+    assert fee_rate >= 0
+
+
+def test_staking_public_methods_present(fair):
+    staking = fair.staking
+    public_attrs = [
+        'add_allowed_receiver',
+        'remove_allowed_receiver',
+        'send_all_fees',
+        'claim_all_fees',
+        'set_fee_rate',
+        'claim_fees',
+        'send_fees',
+        'get_earned_fee_amount',
+    ]
+    for attr in public_attrs:
+        assert hasattr(staking, attr)
