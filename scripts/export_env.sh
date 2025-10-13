04de624ee015e93e2f423d3aa2ce5629d9ee2987
Dmytro <dmitry.tkk@gmail.com>
2025-10-13 16:17:01 +0100

    Replace hardhat submodule with helper scripts command

--- scripts/export_env.sh
+++ scripts/export_env.sh
@@ -3,3 +3,5 @@ export MANAGER_TAG=1.12.0-develop.21
 export ALLOCATOR_TAG=2.2.2-develop.0
 export FAIR_TAG=0.0.1-develop.71
 export MINING_INTERVAL=10
+export MAINNET_ENDPOINT=http://127.0.0.1:8545
+export CHAIN_NAME=fair
