eb814b141f46209572d0f9baa8de883dd56341dc
Dmytro <dmitry.tkk@gmail.com>
2025-12-30 18:05:48 +0000

    Use anvil with saved state

--- scripts/export_env.sh
+++ scripts/export_env.sh
@@ -7,3 +7,4 @@ export FAIR_TAG=0.0.1-develop.71
 export MINING_INTERVAL=10
 export MAINNET_ENDPOINT=http://127.0.0.1:8545
 export CHAIN_NAME=fair
+export ETH_PRIVATE_KEY=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 # anvil key 0 - testing key
